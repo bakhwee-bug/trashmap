29aeb680e4dbd7f5634608a54648e2e0faadee4f
bakhwee-bug <parksy8103@gmail.com>
2022-11-05 03:47:10 +0900

    현재위치 가져오기, Add 버튼 위치 수정

--- app/src/main/java/com/example/trash/MainActivity.kt
+++ app/src/main/java/com/example/trash/MainActivity.kt
@@ -2,27 +2,46 @@ package com.example.trash
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.graphics.Color
+import android.location.Location
+import android.location.LocationRequest
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.annotation.UiThread
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.Transformations.map
+import com.google.android.gms.location.*
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import com.google.android.material.navigation.NavigationView
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
 import com.naver.maps.map.overlay.Marker
+
+
+var permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+//권한 가져오기
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
     OnMapReadyCallback {
 
     private lateinit var navigationView: NavigationView
     private lateinit var drawerLayout: DrawerLayout
-
     private lateinit var viewDetail: ConstraintLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +50,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+
 
         supportActionBar?.run {
             setDisplayHomeAsUpEnabled(true)
@@ -49,6 +70,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             }
 
         mapFragment.getMapAsync(this)
+
+
 
         viewDetail = findViewById(R.id.view_main_detail)
 
@@ -70,7 +93,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         })
 
         val closeDetailButton = findViewById<ImageView>(R.id.bt_main_close_detail)
-        closeDetailButton.setOnClickListener { viewDetail.visibility = View.GONE }
+        closeDetailButton.setOnClickListener {
+            viewDetail.visibility = View.GONE
+            addButton.show()
+        }
 
 
 
@@ -78,11 +104,30 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     }
 
+    fun isPermitted(): Boolean {
+        for (perm in permissions) {
+            if (ContextCompat.checkSelfPermission(this, perm) != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
+    }//권한을 허락 받아야함
+
+    fun startProcess(){
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map_fragment) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map_fragment, it).commit()
+            } //권한
+        mapFragment.getMapAsync(this)
+    } //권한이 있다면 onMapReady연결
+
 
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
+
                 drawerLayout.openDrawer(GravityCompat.END)
             }
             else -> {}
@@ -100,6 +145,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onMapReady(map: NaverMap) {
+
+
         val marker = Marker()
         marker.position = LatLng(37.5670135, 126.9783740)
         marker.map = map
@@ -114,7 +161,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 ).animate(CameraAnimation.Easing)
             )
             viewDetail.visibility = View.VISIBLE
+            val addButton = findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.btn_add)
+            addButton.hide()
             true
         }
     }
+
+
+
 }
