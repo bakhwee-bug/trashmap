4ed13c724142476a737305775e0e7c33318edf2f
bakhwee-bug <parksy8103@gmail.com>
2022-11-13 02:18:11 +0900

    알림창 바꿈2

--- app/src/main/java/com/example/trash/JoinActivity.kt
+++ app/src/main/java/com/example/trash/JoinActivity.kt
@@ -1,5 +1,6 @@
 package com.example.trash
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -18,32 +19,49 @@ class JoinActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
 
+
+        val eventHandler = object : DialogInterface.OnClickListener{
+            override fun onClick(p0: DialogInterface?, p1: Int) {
+                if(p1== DialogInterface.BUTTON_POSITIVE){
+                    val intent = Intent(this@JoinActivity, LoginActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
+
         var retrofit = Retrofit.Builder()
             .baseUrl("http://ec2-3-39-194-139.ap-northeast-2.compute.amazonaws.com:3000/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         var joinService : JoinService = retrofit.create(JoinService::class.java)
 
+        //중복확인
         btn_check.setOnClickListener{
             val email = user_email.text.toString()
             joinService.requestRegister(email).enqueue(object: Callback<Login>{
                 override fun onFailure(call: Call<Login>, t: Throwable) {
-
                     Log.e("JOIN", t.message.toString())
-                    var dialog = AlertDialog.Builder(this@JoinActivity)
-                    dialog.setTitle("false")
-                    dialog.setMessage("이미 가입한 이메일입니다.")
-                   dialog.show()
+                    AlertDialog.Builder(this@JoinActivity).run {
+                        setTitle("false")
+                        setMessage("이미 가입한 이메일입니다.")
+                        setNegativeButton("확인", null)
+                        show()
+                        finish()
+                    }
                 }
+
 
                 override fun onResponse(call: Call<Login>, response: Response<Login>) {
                     val login = response.body()
                     Log.d("JOIN","msg : "+login?.message)
                     Log.d("JOIN","result : "+login?.result)
-                    var dialog = AlertDialog.Builder(this@JoinActivity)
-                    dialog.setTitle(login?.result)
-                    dialog.setMessage(login?.message)
-                    dialog.show()
+
+                    AlertDialog.Builder(this@JoinActivity).run {
+                        setTitle(login?.result)
+                        setMessage(login?.message)
+                        setNegativeButton("확인", null)
+                        show()
+                    }
                 }
             })
         }
@@ -51,43 +69,54 @@ class JoinActivity : AppCompatActivity() {
 
         var authNum =""
 
+        //인증번호 전송
         btn_send.setOnClickListener{
             val email = user_email.text.toString()
             joinService.requestEmail(email).enqueue(object: Callback<Msg>{
                 override fun onFailure(call: Call<Msg>, t: Throwable) {
 
                     Log.e("JOIN", t.message.toString())
-                    var dialog = AlertDialog.Builder(this@JoinActivity)
-                    dialog.setTitle("false")
-                    dialog.setMessage("이미 가입한 이메일입니다.")
-                    dialog.show()
+                    AlertDialog.Builder(this@JoinActivity).run {
+                        setTitle("false")
+                        setMessage(t.message)
+                        setNegativeButton("확인", null)
+                        show()
+                    }
                 }
 
                 override fun onResponse(call: Call<Msg>, response: Response<Msg>) {
                     val login = response.body()
                     Log.d("JOIN","인증번호 : "+login?.message)
                     authNum = login?.message.toString()
-                    var dialog = AlertDialog.Builder(this@JoinActivity)
-                    dialog.setTitle("true")
-                    dialog.setMessage("인증번호가 전송되었습니다")
-                    dialog.show()
+
+                    AlertDialog.Builder(this@JoinActivity).run {
+                        setTitle("true")
+                        setMessage("인증번호가 전송되었습니다")
+                        setNegativeButton("확인", null)
+                        show()
+                    }
                 }
             })
         }
 
+        //인증하기
         btn_cer.setOnClickListener {
             val userAuthNum = user_cernum.text.toString()
             if(userAuthNum==authNum){
-                var dialog = AlertDialog.Builder(this@JoinActivity)
-                dialog.setTitle("true")
-                dialog.setMessage("인증에 성공하였습니다")
-                dialog.show()
+                AlertDialog.Builder(this@JoinActivity).run {
+                    setTitle("true")
+                    setMessage("인증에 성공하였습니다")
+                    setNegativeButton("확인", null)
+                    show()
+                }
             }
             else{
-                var dialog = AlertDialog.Builder(this@JoinActivity)
-                dialog.setTitle("true")
-                dialog.setMessage("인증번호가 틀립니다")
-                dialog.show()
+                AlertDialog.Builder(this@JoinActivity).run {
+                    setTitle("false")
+                    setMessage("인증번호가 틀립니다")
+                    setNegativeButton("확인", null)
+                    show()
+                }
             }
         }
 
@@ -100,25 +129,26 @@ class JoinActivity : AppCompatActivity() {
 
             joinService.requestRP(email, password, nickname).enqueue(object: Callback<Login>{
                 override fun onFailure(call: Call<Login>, t: Throwable) {
-
                     Log.e("JOIN", t.message.toString())
-                    var dialog = AlertDialog.Builder(this@JoinActivity)
-                    dialog.setTitle("false")
-                    dialog.setMessage("회원가입에 실패하였습니다.")
-                    dialog.show()
+                    AlertDialog.Builder(this@JoinActivity).run {
+                        setTitle("false")
+                        setMessage("회원가입에 실패하였습니다.")
+                        setNegativeButton("확인", null)
+                        show()
+                    }
                 }
 
                 override fun onResponse(call: Call<Login>, response: Response<Login>) {
                     val login = response.body()
                     Log.d("JOIN","msg : "+login?.message)
                     Log.d("JOIN","result : "+login?.result)
-                    var dialog = AlertDialog.Builder(this@JoinActivity)
-                    dialog.setTitle(login?.result)
-                    dialog.setMessage(login?.message)
-                    dialog.show()
-                    val intent = Intent(this@JoinActivity, LoginActivity::class.java)
-                    startActivity(intent)
-                    finish()
+
+                    AlertDialog.Builder(this@JoinActivity).run {
+                        setTitle(login?.result)
+                        setMessage(login?.message)
+                        setPositiveButton("확인", eventHandler)
+                        show()
+                    }
                 }
             })
         }
